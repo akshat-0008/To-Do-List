2a9293b8d4a2197654eb31a023b5f67bed16544f
Akshat Pandey <akshatp454git config --global user.email akshatp454@gmail.comgit config --global user.email akshatp454>
2022-06-20 17:34:28 +0530

    Add gitignore, procfile and update ports

--- app.js
+++ app.js
@@ -143,7 +143,6 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen(port);
 
 app.listen(port, function(){
     console.log("Server has started successfully");
